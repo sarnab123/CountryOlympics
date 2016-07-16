8b24b8052ba8b06cc1cdf0db67c60d1b813c4938
tkmagz4 <jui.joshi@kohls.com>
2016-07-15 19:55:33 -0700

    Updating UI for medal tally page.
    - Displaying selected country in first row.
    - Changing the UI for selected country.

--- app/src/main/java/com/olympics/olympicsandroid/view/activity/MedalTallyActivity.java
+++ app/src/main/java/com/olympics/olympicsandroid/view/activity/MedalTallyActivity.java
@@ -13,6 +13,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.olympics.olympicsandroid.R;
@@ -20,10 +21,13 @@ import com.olympics.olympicsandroid.model.ErrorModel;
 import com.olympics.olympicsandroid.model.IResponseModel;
 import com.olympics.olympicsandroid.model.MedalTally;
 import com.olympics.olympicsandroid.model.MedalTallyOrganization;
+import com.olympics.olympicsandroid.networkLayer.cache.database.OlympicsPrefs;
 import com.olympics.olympicsandroid.networkLayer.controller.IUIListener;
 import com.olympics.olympicsandroid.networkLayer.controller.MedalTallyController;
+import com.olympics.olympicsandroid.utility.MedalTallyComparator;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -53,8 +57,19 @@ public class MedalTallyActivity extends AppCompatActivity implements NavigationV
                     false);
             mRecyclerView.setLayoutManager(layoutManager);
             mRecyclerView.setHasFixedSize(true);
+
+            //Set up the rankings for countries based on medal total.
+            List<MedalTallyOrganization> orgsByRanking = new ArrayList<>(medalTallyObj
+                    .getOrganization());
+            if (orgsByRanking != null) {
+                Collections.sort(orgsByRanking, new MedalTallyComparator());
+                for (int index = 0; index < orgsByRanking.size(); index++) {
+                    MedalTallyOrganization orgByStanding = orgsByRanking.get(index);
+                    orgByStanding.setRank(index + 1);
+                }
+            }
             medalTallyListAdapter = new MedalTallyListAdapter();
-            medalTallyListAdapter.setMedalTallyList(medalTallyObj.getOrganization());
+            medalTallyListAdapter.setMedalTallyList(orgsByRanking);
 
             mRecyclerView.setAdapter(medalTallyListAdapter);
         }
@@ -77,11 +92,14 @@ public class MedalTallyActivity extends AppCompatActivity implements NavigationV
         protected void setMedalTallyList(List<MedalTallyOrganization> medalTallyList)
         {
             this.medalTallyList = medalTallyList;
-            Collections.sort(medalTallyList);
+            Collections.sort(medalTallyList, new MedalTallyComparator(OlympicsPrefs.getInstance(null)
+                    .getUserSelectedCountry()));
         }
 
         class ViewHolder extends RecyclerView.ViewHolder
         {
+            private RelativeLayout medalRowLayout;
+            private View dividerView;
             private ImageView countryImageView;
             private TextView countryRankView;
             private TextView countryAlias;
@@ -92,12 +110,16 @@ public class MedalTallyActivity extends AppCompatActivity implements NavigationV
             public ViewHolder(View itemView) {
                 super(itemView);
 
+
+                medalRowLayout = (RelativeLayout)itemView.findViewById(R.id.medal_row_layout);
+                dividerView = (View)itemView.findViewById(R.id.divider_view);
                 countryRankView = (TextView)itemView.findViewById(R.id.id_country_rank);
                 countryAlias = (TextView)itemView.findViewById(R.id.id_country_name);
                 goldCountText = (TextView)itemView.findViewById(R.id.id_gold_count);
                 silverCountText = (TextView)itemView.findViewById(R.id.id_silver_count);
                 bronzeCountText = (TextView)itemView.findViewById(R.id.id_bronze_count);
                 countryImageView = (ImageView)itemView.findViewById(R.id.id_country_image);
+
             }
         }
 
@@ -113,10 +135,16 @@ public class MedalTallyActivity extends AppCompatActivity implements NavigationV
         @Override
         public void onBindViewHolder(MedalTallyListAdapter.ViewHolder holder, int position)
         {
+            //Change row for selected country
+            if (position == 0) {
+                holder.medalRowLayout.setBackgroundResource(R.drawable.medal_first_row_border);
+                holder.dividerView.setVisibility(View.GONE);
+            }
+
             MedalTallyOrganization medalTallyObj = medalTallyList.get(position);
             holder.countryAlias.setText(medalTallyObj.getCountryName());
             holder.countryRankView.setText(Html.fromHtml("<b>" + getResources().getString(R.string
-                    .rank_str, +(position + 1) + "</b>", getResources().getString(R.string
+                    .rank_str, + medalTallyObj.getRank() + "</b>", getResources().getString(R.string
                     .medal_total_str), medalTallyObj.getTotal())));
             holder.goldCountText.setText(medalTallyObj.getGold());
             holder.silverCountText.setText(medalTallyObj.getSilver());
